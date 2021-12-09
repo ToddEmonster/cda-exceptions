6ac51ad2bee83cb8901b86a6561e762f85aa8859
Todd Raharivelo <t.raharivelo@outlook.fr>
2021-12-09 17:12:38 +0100

    feat: display

---
+++ src/exceptions/tp/DefaultCountry.java
@@ -0,0 +1,9 @@
+package exceptions.tp;
+
+public class DefaultCountry {
+    private String name;
+
+    public DefaultCountry(String name) {
+        this.name = name;
+    }
+}
--- src/exceptions/tp/Main.java
+++ src/exceptions/tp/Main.java
@@ -5,18 +5,46 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    private static ArrayList<Country> countries;
 
     public static void main(String[] args) {
-        Country germany = new Country("Allemagne");
-        Country austria = new Country("Autriche");
+        Country allemagne = new Country("Allemagne");
+        Country autriche = new Country("Autriche");
         Country france = new Country("France");
         Country usa = new Country("USA");
         Country madagascar = new Country("Madagascar");
 
-        ArrayList<Country> countries = new ArrayList<Country>(Arrays.asList(
-                germany, austria, france, usa, madagascar
+        countries = new ArrayList<>(Arrays.asList(
+                allemagne, france, autriche, usa, madagascar
         ));
 
-        System.out.println(String.format("La collection crée contient %s pays !", countries.size()));
+        System.out.println("\na)");
+        displayNbCountries();
+
+        System.out.println("\nb)");
+        displayCountries();
+
+        System.out.println("\nc)");
+        countries.clear();
+        displayCountries();
+
+        System.out.println("\nd)");
+        countries = new ArrayList<>(Arrays.asList(
+                allemagne, france, autriche, usa, madagascar
+        ));
+        madagascar.setName("Madagasikara");
+        displayCountries();
+    }
+
+    private static void displayNbCountries() {
+        System.out.println(String.format("La collection créée contient %s pays !", countries.size()));
+    }
+    private static void displayCountries() {
+        if (countries.size() == 0) {
+            System.out.println("Liste vide");
+        } else {
+            displayNbCountries();
+            countries.forEach(c -> System.out.println(c));
+        }
     }
 }
